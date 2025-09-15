50ada54f44e3e53f3c5117be1c401ca4fe22e6a3
Annie Lehmann <annieee.leh@gmail.com>
2025-09-15 13:07:45 -0300

    se crean las funciones para agregar amigos y actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Importante a tener en cuenta, poder ingresar varios nombres, validación de datos, 
+// mostrar un nombre ne pantalla al sortear amigos. 
+// Voy a usar, arrays, funciones, variables, alert, randomizar y botones funcionales.
+let amigos = [];
+
+function agregarAmigo(){
+    let nuevoAmigo = document.getElementById("amigo");
+    
+    if (nuevoAmigo.value.length > 1) {
+        amigos.push(nuevoAmigo.value);
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+    
+    nuevoAmigo.value = '';
+
+    actualizarAmigos()
+}
+
+   
+
+function actualizarAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""
+
+    for (let x = 0; x < amigos.length; x++) {
+        listaAmigos.innerHTML = listaAmigos.innerHTML + '<li>' + amigos[x] + '</li>'
+    }
+}
+
+
